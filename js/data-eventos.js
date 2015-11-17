dde8a9fd86c497cdd148788451b52680750c1284
letmix <mp3edw@gmail.com>
2015-11-17 11:48:39 -0500

    Archivos para ui de cleaningapp

---
+++ js/data-eventos.js
@@ -0,0 +1,10 @@
+var dataEvents = {
+	'11-16-2015' : '<span>Este es una prueba</span>',
+	'11-17-2015' : '<a href="#">Reserva Info</a>',
+	'11-18-2015' : '<span>Info para otra cosa</span>',
+	'11-19-2015' : '<span>Lo que sea corazon</span>',
+	'11-20-2015' : '<span>Este es una prueba</span>',
+	'11-21-2015' : '<a href="#">Reserva Info</a>',
+	'11-22-2015' : '<span>Info para otra cosa</span>',
+	'11-23-2015' : '<span>Lo que sea corazon</span>',
+};
